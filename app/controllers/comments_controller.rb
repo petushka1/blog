4613442e0009d341698774831fb1d85951a74ea7
petushka1 <nadya@devery.io>
2022-10-14 20:47:37 +0300

    add forms for new post, comment, like

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    respond_to do |r|
+      r.html do
+        comment.save
+        redirect_to user_posts_path(current_user)
+      end
+    end
+  end
+
+  def comment_params
+    params
+      .require(:comment)
+      .permit(:text)
+      .merge(author: current_user, post_id: params.require(:post_id))
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,33 @@ class PostsController < ApplicationController
     @post = Post.where(author_id: params[:user_id], id: params[:id]).first
     @comments = Comment.where(post: @post)
   end
+
+  def new
+    post = Post.new
+    respond_to do |r|
+      r.html { render :new, locals: { post:, author: current_user } }
+    end
+  end
+
+  def create
+    post = Post.new(post_params)
+    respond_to do |r|
+      r.html do
+        if post.save
+          flash[:success] = 'Post saved successfully!'
+          redirect_to user_path(current_user)
+        else
+          flash.now[:error] = 'Error: Post wasn\'t saved'
+          render :new, status: :unprocessable_entity, locals: { post:, author: current_user }
+        end
+      end
+    end
+  end
+
+  def post_params
+    params
+      .require(:post)
+      .permit(:title, :text)
+      .merge(author: current_user, comments_counter: 0, likes_counter: 0)
+  end
 end

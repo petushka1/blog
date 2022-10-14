92c6868351458366ce9d27a4ef44d84bac42af55
petushka1 <nadya@devery.io>
2022-10-14 23:17:36 +0300

    fix creator issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
+    post = current_user.posts.new(post_params)
     respond_to do |r|
       r.html do
         if post.save

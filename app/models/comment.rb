bbbca316f02a035fd01a854ffc02052ede626838
petushka1 <nadya@devery.io>
2022-10-21 22:25:35 +0300

    refactor code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,13 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   after_create :increment_comments_counter
+  after_destroy :decrement_comments_counter
 
   def increment_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrement_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
